b51709502bb00f8b41541d8233d90a1e72163f9a
gbs4ever <gbs4ever2@gmail.com>
2019-06-13 17:45:42 -0400

    updated session controller

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,43 @@
+class SessionsController < ApplicationController
+    def create
+      @user = User.find_by(email: params[:session][:email])
+    if@user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    else 
+      render json:{
+        error: "INVALID CREDEN"
+      }
+    
+    end
+  end
+  def getuser
+ # binding.pry
+    if  logged_in?
+    render json: current_user
+    else 
+      render json:{
+        error: "You are not logged in"
+      }
+      
+    end
+  end  
+
+
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
