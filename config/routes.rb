0cca301ecc0bb3e57e6eb8615124669e7c5638ca
gbs4ever <gbs4ever2@gmail.com>
2019-06-12 22:35:24 -0400

    add plate route json

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
+    resources :plates , only: [:index,:show]
+     resources :violation , only: [:index,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
