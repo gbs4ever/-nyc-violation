6ca2066d6527f91bf9d03b675f8d858b88f6bb9a
gbs4ever <gbs4ever2@gmail.com>
2019-06-13 21:52:33 -0400

    added logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
    resources :sessions , only: [:create,:show]
    get '/profile', to: 'sessions#getuser'
-  resources :users
+   delete '/logout', to: 'sessions#destroy'
+  resources :users , only: [:create,:show]
     resources :plates , only: [:index,:show]
      resources :violation , only: [:index,:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
