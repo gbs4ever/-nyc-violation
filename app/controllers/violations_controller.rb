f18fdad1135b38f3c74b1c8fae5819fda09468d9
gbs4ever <gbs4ever2@gmail.com>
2019-06-14 17:47:56 -0400

    parsae json from api correctly

--- app/controllers/violations_controller.rb
+++ app/controllers/violations_controller.rb
@@ -1,18 +1,40 @@
 class ViolationsController < ApplicationController
  
- ##1 will return array of 3 json objects
- ## 2 we want to send that data to redux
- ## 3 save params to plate model 
-## user should be able to search and save there plate 
+ ##1 will return array of 3 json objects- done
+ ## 2 we want to send that data to redux 
+ ## 3 save params to plate model  - saves
+## user should be able to search and save there plate we need to add fetch request and send back there   
 ##  
 
-def index
- params[:palate]
-current_user.plates.build(parmas)
+def search
+state=params[:violation][:state].upcase
+    @resp = Faraday.get('https://data.cityofnewyork.us/resource/uvbq-3m68.json') do |req|
+  req.params['plate'] = params[:violation][:number]
+  req.params['state'] = state
+  req.params['$limit'] = 10 
+    end
+  
+  if @resp.success?
+    body = JSON.parse(@resp.body)
+    if body.empty?
+      
+       render json:{
+        notice: "Yay No tickets"
+      } and return
+ 
+      ##need to break 
+    else
+      user= current_user.plates.build(number: params[:violation][:number], state: params[:violation][:state])
+      current_user.save
+      render json: body
+    end
+  else
+    render json:{
+        error: "Api failed "
+      }
+  end
+ 
 
- @resp=get 'https://data.cityofnewyork.us/resource/uvbq-3m68.json'
-# make sure it renders in json 
- render json: @resp
 end
 
 
