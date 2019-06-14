b0040fda85da556584f11e08a8bf6c98237f21f4
gbs4ever <gbs4ever2@gmail.com>
2019-06-14 18:00:08 -0400

    violations reducer

---
+++ src/reducers/violations.js
@@ -0,0 +1,18 @@
+export default (state = [], action) => {
+
+  switch (action.type) {
+    case "ADD_VIOLATION":
+      
+      return [...state,action.tickets] 
+
+
+    default:
+      return state
+  
+
+
+
+
+  }
+
+}
