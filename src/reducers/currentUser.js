63f9b5a5f79e068baf2f38500eba022ab603aa4f
gbs4ever <gbs4ever2@gmail.com>
2019-06-13 21:56:42 -0400

    session cookie reducer

---
+++ src/reducers/currentUser.js
@@ -0,0 +1,22 @@
+export default (state = null, action) => {
+
+  switch (action.type) {
+    case "SET_CURRENT_USER":
+    
+      return action.user
+    case "CLEAR_CURRNET_USER":
+     
+     return null
+   
+
+
+    default:
+      return state
+
+
+
+
+
+  }
+
+}
