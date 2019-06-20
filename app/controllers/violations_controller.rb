aec4903514ecdaf524c0396914ef3a6de1dcc975
gbs4ever <gbs4ever2@gmail.com>
2019-06-20 02:05:56 -0400

    added building violations

--- app/controllers/violations_controller.rb
+++ app/controllers/violations_controller.rb
@@ -1,19 +1,14 @@
 class ViolationsController < ApplicationController
  before_action :confirm_logged_in?
- ##1 will return array of 3 json objects- done
- ## 2 we want to send that data to redux 
- ## 3 save params to plate model  - saves
-## user should be able to search and save there plate we need to add fetch request and send back there   
-##  
 
-def search
+  def search
 
-state=params[:violation][:state].upcase
-    @resp = Faraday.get('https://data.cityofnewyork.us/resource/uvbq-3m68.json') do |req|
-  req.params['plate'] = params[:violation][:number]
-  req.params['state'] = state
-  req.params['$limit'] = 15 
-    end
+  state=params[:violation][:state].upcase
+  @resp = Faraday.get('https://data.cityofnewyork.us/resource/uvbq-3m68.json') do |req|
+    req.params['plate'] = params[:violation][:number]
+    req.params['state'] = state
+    req.params['$limit'] = 15 
+  end
    user = current_user
    user.plates.build(number: params[:violation][:number], state: state)
      user.save
@@ -38,31 +33,33 @@ state=params[:violation][:state].upcase
         error: "Api failed "
       }
   end
- def buildings
-  @resp = Faraday.get(' https://data.cityofnewyork.us/resource/dvnq-fhaa.json?') do |req|
-
-  req.params['house_number'] = params[:violation][:number]
-  req.params['street'] = state
-  req.params['$limit'] = 20
-    end
-    if @resp.success?
-    body = JSON.parse(@resp.body)
-    if body.empty?  
-       render json:{
-        notice: "Yay No Violations"
-      } and return
-    else
-      render json: body
-    end
-    else
-    render json:{
-        error: "Api failed "
-      }
-    end
-binding.pry
-end 
-
 end
+  def buildings
+  street=params[:violation][:street].upcase
+    @resp = Faraday.get('https://data.cityofnewyork.us/resource/dvnq-fhaa.json?') do |req|
+      req.params['house_number']  = params[:violation][:houseNumber]
+      req.params['street'] = street
+      req.params['$limit'] = 20
+    end
+      
+      if @resp.success?
+        body = JSON.parse(@resp.body)
+          if body.empty?  
+            render json:{
+              notice: "No results please try searching again"
+            } and return
+          else
+            render json: body
+          end
+        else
+       render json:{
+          error: "Api failed "
+        }
+      end
+
+    end 
+
+
 
 
 
@@ -80,18 +77,20 @@ end
 
 
 private
-def confirm_logged_in?
-
-if  !current_user
-render json:{
-        error: "Please login in to search"
-      }
-end  
-
-end
+    def confirm_logged_in?
+      if  !current_user
+      render json:{
+              error: "Please login in to search"
+            }
+      end  
+    end
 
 
 
+# Only allow a trusted parameter "white list" through, if saving to database 
+  #  def violation_params
+  #    params.require(:violation).permit(:number, :state)
+  #  end
 
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
    resources :sessions , only: [:create,:show]
     get '/profile', to: 'sessions#getuser'
-    post '/buildings', to: 'violations#buildings'
+    post'/buildings', to: 'violations#buildings'
+     
     post '/violations', to: 'violations#search'
     delete '/logout', to: 'sessions#destroy'
   
