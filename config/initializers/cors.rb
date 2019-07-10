a61fbe3457cd766456d973c59c2e616944b062b0
gbs4ever <gbs4ever2@gmail.com>
2019-07-10 10:43:09 -0400

    cors policy update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'romantic-torvalds-8beb52.netlify.com/'
+    origins 'https://romantic-torvalds-8beb52.netlify.com'
 
      resource '*',
       
