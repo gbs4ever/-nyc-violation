e2e50b6d4e631fb50d9500125452f7cb9ebb3b28
gbs4ever <gbs4ever2@gmail.com>
2019-07-10 00:44:31 -0400

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'romantic-torvalds-8beb52.netlify.com/'
 
      resource '*',
       
