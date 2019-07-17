51c969898b51c04a3134b69b8c76263d8ae02e82
gbs4ever <gbs4ever2@gmail.com>
2019-07-16 22:42:13 -0400

    url cors errors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins '*'
 
-     resource '*',
+     resource 'https://condescending-murdock-bc56f4.netlify.com/',
       
        headers: :any,
        methods: [:get, :post, :put, :patch, :delete, :options, :head],
