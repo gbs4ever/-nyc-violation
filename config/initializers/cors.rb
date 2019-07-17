4f600f0ab9ca305dd439a756453d3369aa9b858c
gbs4ever <gbs4ever2@gmail.com>
2019-07-16 22:31:44 -0400

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'condescending-murdock-bc56f4.netlify.com/'
+    origins '*'
 
      resource '*',
       
