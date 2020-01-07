c2370357e0b82ccdcb84e39493911908ac62b46d
gbs4ever <gbs4ever2@gmail.com>
2020-01-07 15:36:01 -0500

    fixed cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,11 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-<<<<<<< HEAD
   origins 'nyc-violation.netlify.com/'
-=======
-    origins 'nyc-violation.netlify.com'
->>>>>>> 80aef923b3046bc31e39c0a5728f04ab60668998
 
      resource '*',
       
